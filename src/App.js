e0120861957ad4f46b705d00e466d9da16f98cd6
Shigeyuki Isorena <Isorenashigeyuki21@gmail.com>
2023-06-08 23:36:27 -0700

    hw

--- src/App.js
+++ src/App.js
@@ -14,8 +14,6 @@ function App() {
   let [data, setData] = useState([]);
   let searchInput = useRef("");
 
-  /*useEffect(() => {}, [search]); */
-
   const handleSearch = (e, search) => {
     e.preventDefault();
     const fetchData = async () => {
--- src/components/ArtistView.jsx
+++ src/components/ArtistView.jsx
@@ -1,13 +1,50 @@
 import { useEffect, useState} from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link, useNavigate } from 'react-router-dom'
 
 export default function ArtistView(){
+    const navigate = useNavigate()
     const { id } = useParams()
     const [artistData, setArtistData] = useState([])
+
+    useEffect(() => {
+        const API_URL  = `http://localhost:4000/album/${id}`
+        const fetchData = async () =>{
+            const respone = await fetch(API_URL)
+            const resData = await respone.json()
+            setArtistData(resData.results)
+         }
+         fetchData();
+    }, [id]);
+
+    const justAlbum = artistData.filter(entry => entry.collectionType === 'Album');
+
+    const renderAlbums =justAlbum.map((album, i) => {
+        return <div key={i}>
+            <Link to={`/album/${album.collectionId}`}>
+                <p>{album.collectionName}</p>
+            </Link>
+        </div>
+    })
+    
+    const navButtons = () => {
+        return <div>
+            <button type='Button' onClick={() => navigate(-1)}>Back</button>
+            <button type='button' onClick={() => navigate('/')}>Home</button>
+        </div>
+    }
+
+    const showAristName = () => {
+        return artistData.length ? 
+        <h3>{artistData[0].artistName}</h3>
+        :
+        <h3>Loading...</h3>
+    }
+
     return (
         <div>
-            <h2>The id passed is: { id }</h2>
-            <p>Artist Data goes here</p>
+            {showAristName() }
+            { navButtons() }
+            { renderAlbums }
         </div>
     )
 }
